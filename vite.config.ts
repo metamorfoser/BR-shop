3fb9ef55c7a61155db01a73d9405aa21d8e51374
malin <malin4690@gmail.com>
2026-02-03 17:29:08 +0100

    Med GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Viktigt: base måste matcha ditt GitHub-repo-namn
+// Annars fungerar inte routing när du publicerar på GitHub Pages.
 export default defineConfig({
+  base: '/BR-shop/',
+
   server: {
-    open: true,   //  öppnar webbläsaren automatiskt
-    port: 5173    //  ändra port här
+    open: true,   // öppnar webbläsaren automatiskt
+    port: 5173    // port för dev-servern
   },
+
   plugins: [react()],
 })
